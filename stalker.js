334a32e8307b52ccff5f13cc9f1de0b0f1a48562
Jay <thefreshbegal@gmail.com>
2018-06-15 16:09:05 -0700

    Add stalker.html

---
+++ stalker.js
@@ -0,0 +1,7 @@
+let userFirstName = prompt("What is your first name?");
+let userLastName = prompt("What is your last name?");
+let userAge = prompt("What is your age?");
+
+console.log("Your first name is " + userFirstName);
+console.log("Your last name is " + userLastName);
+console.log("Your age is " + userAge);
