ca5f71f6c9af009e9a4a1ebfe3c167e9b99cbb0c
jegalchoi <thefreshbegal@gmail.com>
2018-07-25 15:00:06 -0700

    Add array_iteration.html and array_iteration.js

---
+++ array_iteration.js
@@ -0,0 +1,9 @@
+var numbers = [0,1,2,3,4,5,6,7,8.9,10];
+
+var colors = ["orange","yellow","green"];
+
+for(var i = 0; i <= numbers.length; i++) {
+  if(i % 3 === 0) {
+    console.log(numbers[i]);
+  }
+}
